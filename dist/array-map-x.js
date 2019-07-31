57f9e3ffb5a6bdfa9696ba16e0624baf316cb5c4
Graham Fairweather <xotic750@gmail.com>
2019-07-31 19:33:16 +0200

    Package rework

--- dist/array-map-x.js
+++ dist/array-map-x.js
@@ -2,7 +2,7 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017",
-  "date": "2019-07-31T16:03:58.295Z",
+  "date": "2019-07-31T17:33:10.234Z",
   "describe": "",
   "description": "Creates an array with the results of calling a function on every element.",
   "file": "array-map-x.js",
