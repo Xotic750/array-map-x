8a7d1e1a7733c1be2538fbe941f39d310df90a52
Graham Fairweather <xotic750@gmail.com>
2019-08-05 19:23:04 +0200

    Auto build

--- dist/array-map-x.js
+++ dist/array-map-x.js
@@ -2,7 +2,7 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017",
-  "date": "2019-08-05T15:46:18.914Z",
+  "date": "2019-08-05T17:22:58.641Z",
   "describe": "",
   "description": "Creates an array with the results of calling a function on every element.",
   "file": "array-map-x.js",
