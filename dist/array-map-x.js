8f1633beb9138f678434ab67f01d5d555989e078
Graham Fairweather <xotic750@gmail.com>
2019-08-06 02:43:19 +0200

    :bookmark: v3.0.22
    Coveralls

--- dist/array-map-x.js
+++ dist/array-map-x.js
@@ -2,7 +2,7 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017",
-  "date": "2019-08-05T18:34:47.573Z",
+  "date": "2019-08-06T00:43:13.398Z",
   "describe": "",
   "description": "Creates an array with the results of calling a function on every element.",
   "file": "array-map-x.js",
