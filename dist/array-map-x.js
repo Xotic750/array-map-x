7fa04606aadc5383f925e8eb85035a7cca9f60b8
Graham Fairweather <xotic750@gmail.com>
2019-08-06 23:09:55 +0200

    Add coveralls badge

--- dist/array-map-x.js
+++ dist/array-map-x.js
@@ -2,7 +2,7 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017",
-  "date": "2019-08-06T00:43:13.398Z",
+  "date": "2019-08-06T21:09:48.965Z",
   "describe": "",
   "description": "Creates an array with the results of calling a function on every element.",
   "file": "array-map-x.js",
